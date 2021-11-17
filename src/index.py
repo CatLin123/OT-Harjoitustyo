a8964d1bce1e6528037909fc9141e281d7c8162b
Linda B <bjorkenheimlinda@gmail.com>
2021-11-17 12:40:40 +0200

    initial commit

---
+++ src/index.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+
+
+def main():
+    window = tk()
+    window.title("Budget app")
+    window.geometry("600x400")
+
+    window.mainloop()
+    
+
+if __name__ == '__main__':
+    main()
